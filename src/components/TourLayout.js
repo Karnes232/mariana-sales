04a2235bf7d35aaf7dd16c3ee0eb8ce77a7fe3b8
James Karnes <karnes.james@gmail.com>
2022-08-13 14:01:00 -0400

    trying call button

--- src/components/TourLayout.js
+++ src/components/TourLayout.js
@@ -9,7 +9,8 @@ const TourLayout = ({tour}) => {
   return (
     <Layout>
       <div className="w-screen max-w-lg flex flex-col my-5 items-center">
-      {tour.gallery ? (
+      
+      {tour.gallery && 
         <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true}>
           {tour.gallery.map((image, index) => (
             <div className="h-60" key={index}>
@@ -17,34 +18,45 @@ const TourLayout = ({tour}) => {
             </div>
           ))}
         </Carousel>
-        ): (<></>)}
+        }
         <div className="flex flex-col items-center justify-center mt-5 space-y-2 mx-4">
-        {tour.pageName ? (
-          <h1 className="text-2xl font-semibold">{tour.pageName}</h1> ): (<></>)}
-          {tour.descriptionMain ? (<p className="text-sm text-gray-800 text-center">
-            {tour.descriptionMain}
-          </p>  ): (<></>)}
-        </div>
-        <div className="flex flex-col items-center justify-center mt-5 space-y-1 mx-4">
-          <h1 className="text-xl font-semibold">Trip Overview</h1>
-          {tour.price ? (<h4 className="text-sm text-gray-500">Price: ${tour.price}</h4> ): (<></>)}
-          {tour.price2 ? (<h4 className="text-sm text-gray-500">Price: ${tour.price2}</h4> ): (<></>)}
-          {tour.price3 ? (<h4 className="text-sm text-gray-500">Price: ${tour.price3}</h4> ): (<></>)}
-          {tour.overview ? (<h4 className="text-sm text-gray-500">{tour.overview}</h4> ): (<></>)}
-        </div>
-        <div className="flex flex-col items-center justify-center mt-5 mx-4 space-y-2">
-        {tour.descriptionSecondary ? (<p className="text-sm text-gray-800 text-center">
-            {tour.descriptionSecondary}
-          </p> ): (<></>)}
-        </div>
         <a
           href={`https://api.whatsapp.com/send?phone=${process.env.PHONE_NUMBER}`}
           target="_blank"
           aria-label="Whatsapp"
           rel="noreferrer"
         >
-          <button className="uppercase text-sm font-bold tracking-wide bg-[#25D366] text-gray-100 p-3 rounded-lg w-80 my-6 focus:outline-none focus:shadow-outline">
+          <button className="uppercase text-sm font-bold tracking-wide bg-[#25D366] text-gray-100 p-3 rounded-lg w-80 my-1 focus:outline-none focus:shadow-outline">
             WhatsApp
+            <FaWhatsapp className="inline-block h-4 w-5 ml-1" />
+          </button>
+        </a>
+        {tour.pageName && 
+          <h1 className="text-2xl font-semibold">{tour.pageName}</h1>}
+          {tour.descriptionMain && <p className="text-sm text-gray-800 text-center">
+            {tour.descriptionMain}
+          </p> }
+        </div>
+        <div className="flex flex-col items-center justify-center mt-5 space-y-1 mx-4">
+          <h1 className="text-xl font-semibold">Trip Overview</h1>
+          {tour.price && <h4 className="text-sm text-gray-500">Price: ${tour.price}</h4> }
+          {tour.price2 && <h4 className="text-sm text-gray-500">Price: ${tour.price2}</h4> }
+          {tour.price3 && <h4 className="text-sm text-gray-500">Price: ${tour.price3}</h4> }
+          {tour.overview && <h4 className="text-sm text-gray-500">{tour.overview}</h4>}
+        </div>
+        <div className="flex flex-col items-center justify-center mt-5 mx-4 space-y-2">
+        {tour.descriptionSecondary && <p className="text-sm text-gray-800 text-center">
+            {tour.descriptionSecondary}
+          </p> }
+        </div>
+        <a
+          href={`tel:5554280940${process.env.PHONE_NUMBER}`}
+          target="_blank"
+          aria-label="Whatsapp"
+          rel="noreferrer"
+        >
+          <button className="uppercase text-sm font-bold tracking-wide bg-[#0c16d9] text-gray-100 p-3 rounded-lg w-80 my-6 focus:outline-none focus:shadow-outline">
+            Call Me
             <FaWhatsapp className="inline-block h-4 w-5 ml-1" />
           </button>
         </a>
