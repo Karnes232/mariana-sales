d658e3da17984a1b41c725a842d544e3b07eab87
James Karnes <karnes.james@gmail.com>
2022-08-23 20:16:56 -0400

    updated tour list

--- src/data/longerTours.js
+++ src/data/longerTours.js
@@ -16,18 +16,59 @@ export const longerTours = [
     ],
     overview: "1.5 Hours Tour",
     descriptionMain:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum iure saepe consequatur nihil excepturi quo molestias nemo a in, fugiat natus minima, esse tempora! Ab at sequi eligendi id saepe velit eius quas quae quia ex, tempora veritatis architecto rerum nihil repudiandae ratione magnam a expedita, eaque, inventore ipsa eos! Cum dolor nam totam laborum pariatur placeat nesciunt veniam ex neque natus iure harum id suscipit nihil sed qui quibusdam, numquam a in veritatis fugit perferendis atque corrupti ipsum. Culpa quisquam nesciunt dolores beatae quasi doloremque nihil voluptate alias velit iusto. Ea illum, quibusdam quam veritatis assumenda saepe ad.",
+      "Adventure lovers—this is the one for you. Swap predictable group sightseeing boat tours for something more hands-on and exhilarating—a chance to take full control of a speedboat and ride along the coast of Punta Cana. During the trip, pause at a coral reef to go snorkeling, visit a beautiful natural pool, and enjoy a break for refreshments",
     descriptionSecondary:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ducimus temporibus velit vero nobis? Excepturi similique hic, facilis obcaecati dolor aperiam nostrum! Sit vero consectetur impedit cum voluptate, maxime blanditiis iure aliquid praesentium voluptates quod, culpa dolores asperiores, veritatis dolore laborum ullam cupiditate voluptatum corporis quam. Quae repellendus ea animi. Deleniti eum nulla blanditiis, quidem recusandae excepturi natus voluptas aperiam a! Libero dolorum unde commodi voluptatibus sunt dolorem exercitationem, error earum itaque rem officia molestias corporis iusto, laboriosam suscipit totam, nesciunt ratione nam eligendi. Nostrum a minus enim perferendis fugiat suscipit at rem aperiam, nulla, sint, consectetur ad ullam ducimus!",
+      "Get ready for an exhilarating adventure in the ocean near Punta Cana. Instead of going to the hassle of booking all your watersports separately, this convenient tour includes a guided speedboat trip and snorkeling equipment rental too. First, get behind the wheel of your own speedboat and whizz across the coastal waters with a guide to lead the way so you don't get lost. Then, visit a fantastic snorkeling spot that's home to tropical fish and coral reefs.",
   },
   {
     id: 2,
-    name: "ScubaDoo",
-    pageName: "ScubaDoo",
-    price: "70 per person",
-    link: "/longer/scubadoo",
+    name: "Fishing",
+    pageName: "Fishing",
+    price: "100 per person",
+    price2: "55 per watcher",
+    link: "/longer/fishing",
+    description: "Deep Sea Trawling",
+    mainImg: "https://i.ibb.co/jz1vbfH/scubadoo3.jpg",
+    gallery: [
+      "https://i.ibb.co/sCgb6rL/scubadoo2.jpg",
+      "https://i.ibb.co/jz1vbfH/scubadoo3.jpg",
+      "https://i.ibb.co/ypZX7Zs/scubadoo.jpg",
+    ],
+    overview: "4 Hour Tour",
+    descriptionMain:
+      "Whether you've never tried fishing before or go every weekend, this four-hour deep sea fishing charter provides fun and relaxation. Travel on a spacious boat with comfortable indoor and outdoor areas. Try your luck at catching blue marlin, Atlantic sailfish, yellowfin tuna, and more.",
+
+    descriptionSecondary:
+      "Get out on the waters off Punta Cana for a deep sea fishing experience, with all equipment included and guidance from an experienced captain. With the captain's knowledge of the area, you'll have the best possible chances of returning to land with a trophy-size catch. Choose how much or how little instruction you'd like to have; which makes this excursion suitable for both beginners and experienced fishermen",
+  },
+  {
+    id: 3,
+    name: "Glass Bottom",
+    pageName: "Glass Bottom",
+    price: "63 per person",
+    link: "/longer/glass",
     description:
-      "Drive an underwater scooter along the seabed of Punta Cana with this incredible Scuba Doo activity",
+      "Get close-up views of tropical fish, coral, and other marine life",
+    mainImg: "https://i.ibb.co/jz1vbfH/scubadoo3.jpg",
+    gallery: [
+      "https://i.ibb.co/sCgb6rL/scubadoo2.jpg",
+      "https://i.ibb.co/jz1vbfH/scubadoo3.jpg",
+      "https://i.ibb.co/ypZX7Zs/scubadoo.jpg",
+    ],
+    overview: "1.5 Hours Tour",
+    descriptionMain:
+      "Sail along our beautiful coast and visit the underwater aquarium without getting wet, book our glass bottom boat tour and enjoy the nice view of the Punta Cana cost with it green palm tree and turquoise water",
+
+    descriptionSecondary:
+      "Get close-up views of tropical fish, coral, and other marine life on this fun, half-day glass-bottomed boat tour. Relax on board as you enjoy the views and take advantage of the provided snacks and drinks. If you don't mind getting wet, don some snorkeling gear and swim at a beautiful natural swimming pool. An ideal trip for the whole family.",
+  },
+  {
+    id: 4,
+    name: "Surf",
+    pageName: "Surf",
+    price: "89 per person",
+    link: "/longer/surf",
+    description: "Punta Cana Surf Adventure",
     mainImg: "https://i.ibb.co/jz1vbfH/scubadoo3.jpg",
     gallery: [
       "https://i.ibb.co/sCgb6rL/scubadoo2.jpg",
@@ -36,8 +77,9 @@ export const longerTours = [
     ],
     overview: "2.5 Hours Tour",
     descriptionMain:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum iure saepe consequatur nihil excepturi quo molestias nemo a in, fugiat natus minima, esse tempora! Ab at sequi eligendi id saepe velit eius quas quae quia ex, tempora veritatis architecto rerum nihil repudiandae ratione magnam a expedita, eaque, inventore ipsa eos! Cum dolor nam totam laborum pariatur placeat nesciunt veniam ex neque natus iure harum id suscipit nihil sed qui quibusdam, numquam a in veritatis fugit perferendis atque corrupti ipsum. Culpa quisquam nesciunt dolores beatae quasi doloremque nihil voluptate alias velit iusto. Ea illum, quibusdam quam veritatis assumenda saepe ad.",
+      "An amazing team of happy, professional and dedicated individuals with one goal: Sharing their love and passion for surfing! Just 15 minutes fom most of the hotels in the Punta Cana area. In the water, one instructor is always with you (sometimes, more than one). Guaranteeing progression based on your needs, not on a rushed schedule.",
+
     descriptionSecondary:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ducimus temporibus velit vero nobis? Excepturi similique hic, facilis obcaecati dolor aperiam nostrum! Sit vero consectetur impedit cum voluptate, maxime blanditiis iure aliquid praesentium voluptates quod, culpa dolores asperiores, veritatis dolore laborum ullam cupiditate voluptatum corporis quam. Quae repellendus ea animi. Deleniti eum nulla blanditiis, quidem recusandae excepturi natus voluptas aperiam a! Libero dolorum unde commodi voluptatibus sunt dolorem exercitationem, error earum itaque rem officia molestias corporis iusto, laboriosam suscipit totam, nesciunt ratione nam eligendi. Nostrum a minus enim perferendis fugiat suscipit at rem aperiam, nulla, sint, consectetur ad ullam ducimus!",
+      "Macao Beach has many surf breaks for different level of surfers. We will always find a spot where you feel comfortable, challenged, and above-all safe! Enjoying your surfing experience and being totally stoked after each session is as important as learning the skills themselves! Surfing in a sport that lifts you up, empowers and motivates you! Your stoke is our goal",
   },
 ]
