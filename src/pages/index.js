94cd1378b828153a0193e71bfa8db232cb4a9ac9
James Karnes <karnes.james@gmail.com>
2023-05-02 20:25:01 -0400

    tacano

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 import { Helmet } from "react-helmet"
 
 const IndexPage = () => {
+  const [paid, setPaid] = useState(false)
   return (
     <Layout>
       <Helmet title="Mariana Caribbean Sports" />
-      <div className="bg-image w-screen max-w-lg ">
+      {paid ? (
+        <div className="bg-image w-screen max-w-lg ">
         <Link to="/diving">
           <button className="bg-image__button top-[calc(50%-120px)] inline-block rounded-md text-center w-48 font-bold text-lg py-4 px-4 mb-20 text-white bg-sky-500 opacity-75 hover:opacity-100 hover:bg-blue-600">
             Scuba Diving
@@ -24,6 +26,10 @@ const IndexPage = () => {
           </button>
         </Link>
       </div>
+      ) : (
+        <div className="bg-image-tacano w-screen max-w-lg "></div>
+      )}
+      
     </Layout>
   )
 }
